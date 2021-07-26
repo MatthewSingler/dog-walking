ee87b3ecb47e1286c70ca1cf9f26f38a3edee80b
Matthew Singler <matthew.singler@gmail.com>
2021-07-26 11:57:43 -0500

    Adding event listener to registeredpets

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,13 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
+
 
 const pets = getPets()
-
+const walkers = getWalkers()
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
@@ -14,3 +15,26 @@ export const RegisteredPets = () => {
     return petHTML
 }
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [, walkerId] = itemClicked.id.split("--")
+            
+            for (const pet of pets) {
+                if (pet.id === parseInt(walkerId)) {
+                    const whosWalkingWho = pets.filter(
+                        (pet) => {
+                            if (pet.walkerId === parseInt(walkerId)) {
+                                return true
+                            }
+                        }
+                    )
+                    window.alert(`${walker.name} is currently walking ${whosWalkingWho.length}`)
+                }
+                
+            }
+        }
+    }
+)
